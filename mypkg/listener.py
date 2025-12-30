69000ac11783275b2a725e3b9e44fa4e640c814d
Makusa Ushu <zuojiujianxiu3@gmail.com>
2025-12-30 12:16:40 +0900

    Initial commit

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -2,15 +2,15 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16
 
-rclpy.int()
-node =Node("lostener")
+rclpy.init()
+node =Node("listener")
 
 
-def cd(msg):
+def cb(msg):
     global node
     node.get_logger().info("Listen: %d" % msg.data)
 
 
 def main():
-    pub = node.create_subscription(int16, "countup", cb, 10)
+    pub = node.create_subscription(Int16, "countup", cb, 10)
     rclpy.spin(node)
