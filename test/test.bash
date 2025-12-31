dc445a1f3065014f212ac11ee9c7041bdc23c6dc
Makusa Ushu <zuojiujianxiu3@gmail.com>
2025-12-31 23:23:30 +0900

    Initial commit

--- test/test.bash
+++ test/test.bash
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 set -e
 
 dir=~
@@ -9,7 +8,6 @@ cd $dir/ros2_ws
 source /opt/ros/humble/setup.bash
 colcon build
 
-
 timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
- 
 grep '角の数10' /tmp/mypkg.log
+
